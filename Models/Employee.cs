5a149b440c1836d28de47f57b618baf5509a255b
NiklasDahlgrens <96531368+NiklasDahlgrens@users.noreply.github.com>
2022-03-16 15:14:44 +0100

    Add files via upload

---
+++ Models/Employee.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeAPI.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int SecNumber { get; set; }
+        public string TelephoneNr { get; set; }
+        public string Role { get; set; }
+    }
+}
